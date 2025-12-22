cd90d680df0ae3e31c98633aa7d44842948e897d
Tomáš Míčka <tomas.micka@logmanager.com>
2025-12-22 10:55:05 +0100

    update

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 require('config.lazy')
 require('config.nvim')
-require('config.functions')
 
 -- local parser_config = require 'nvim-treesitter.parsers'.get_parser_configs()
 -- parser_config.latte = {
@@ -30,29 +29,3 @@ vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
 -- vim.cmd [[hi @keyword.function.lua guifg=pink]]
 
 vim.api.nvim_set_keymap('n', '==', ':lua vim.lsp.buf.format()<CR>', { noremap = true, silent = true })
-
--- require('lspconfig').clangd.setup{
---   cmd = { "/run/current-system/sw/bin/clangd" },  -- path to Nix-installed clangd
--- }
---
----
----- 1. Add fennel.lua location to Lua module search path
-package.path = package.path .. ";"
-  .. "/nix/store/1z42sfd8lc3pnm83kxc8minz4jnjid1i-luajit2.1-fennel-1.5.3-1/share/lua/5.1/?.lua;"
-
-
-local fennel = require("fennel")
-
-fennel.install()
--- conjure completion setup
-package.path = package.path .. ";"
-  .. os.getenv("HOME") .. "/.config/waybar/fnl/?.fnl;"
-  -- .. os.getenv("HOME") .. "/.config/waybar/scripts/?/init.fnl"
-  -- .. os.getenv("HOME") .. "/.config/waybar/fnl/?.fnl;"
-fennel.path = package.path
--- local mylib = require("module")
-
--- local module_path = os.getenv("HOME") .. "/.config/waybar/fnl/module.fnl"
-
--- load and compile the module via fennel
--- local mylib = fennel.dofile(module_path)
