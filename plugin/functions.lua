6c6128b89fc9573de594bc35d675113160117313
Tomáš Míčka <tomas.micka@logmanager.com>
2026-03-25 17:42:40 +0100

    update custome functions + add review mode

--- plugin/functions.lua
+++ plugin/functions.lua
@@ -1,15 +1,52 @@
-vim.api.nvim_create_user_command(
+---@class UserCommandOpts
+---@field keys string
+---@field mode string
+---@field desc string
+
+---@param name string
+---@param fn function
+---@param keymap_opts? UserCommandOpts
+local function create_custom_user_comand(name, fn, keymap_opts)
+    vim.api.nvim_create_user_command(name, function() fn() end, {})
+
+    if keymap_opts then
+        vim.keymap.set(keymap_opts.mode, keymap_opts.keys, fn, { desc = keymap_opts.desc })
+    end
+end
+
+
+create_custom_user_comand(
     "Blame",
     function() require('gitsigns').blame() end,
-    {}
+    nil
 )
 
-vim.api.nvim_create_user_command(
+create_custom_user_comand(
+    "Review",
+    function()
+        local signs = require('gitsigns')
+
+        signs.toggle_linehl()
+        signs.toggle_deleted()
+    end,
+    {
+        mode = 'n',
+        keys = '<leader>gr',
+        desc = 'review mode',
+    }
+);
+
+
+create_custom_user_comand(
     "CopyFile",
     function()
-        local content = vim.fn.getreg('%')
-        vim.fn.setreg('+', content)
+        local filename = vim.fn.expand('%')
+        vim.fn.setreg('+', filename)
+        vim.notify(filename)
     end,
-    {}
+    {
+        mode = 'n',
+        keys = '<leader>fa',
+        desc = 'Copy current file content to clipboard',
+    }
 )
-vim.keymap.set('n', '<leader>fa', ':CopyFile<CR>', { desc = 'Copy current file content to clipboard' })
